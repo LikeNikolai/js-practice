cd39abb50f4fb7effa11115c93f7473f48d81d59
EkaterinaKononenko <kaktysik4@ukr.net>
2022-08-20 09:57:43 +0300

    tasks

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@
 // //число (количество минут) и выведет в консоль
 // //строку в формате часов и минут
 // //
-const mins = prompt("Введіть кількість хвилин.");
+/*const mins = prompt("Введіть кількість хвилин.");
 function timeNormalization(time) {
   const hours = Math.floor(time / 60);
   const minutes = time % 60;
@@ -12,4 +12,27 @@ function timeNormalization(time) {
   return normalizedTime;
 }
 
-console.log(timeNormalization(mins));
+console.log(timeNormalization(mins));*/
+/*const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  let totalPrice = 0;
+  totalPrice = products.price * products.quantity;
+  return propName === products.name ? totalPrice : [];*/
+
+function addOverNum(one, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    total += arg;
+  }
+  return total;
+}
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 // Определите в какую четверть часа попадает
 // это число(в первую, вторую, третью или четвертую).
 
-const numUser = Number(prompt("Ведіть число від 0 до 60."));
+//const numUser = Number(prompt("Ведіть число від 0 до 60."));
 // function quarterChecker(num) {
 //   if (num >= 0 && num <= 15) {
 //     // alert(`Ваше число ${num} знаходиться в першій чверті.`);
@@ -51,3 +51,19 @@ const numUser = Number(prompt("Ведіть число від 0 до 60."));
 // }
 
 // console.log(quarterChecker(numUser));
+/*const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+  for (const product of products) {
+  }
+  // Пиши код выше этой строки
+  if (product === products.name) {
+    calculateTotalPrice = products.price * products.quantity;
+    return calculateTotalPrice;
+  }
+}*/
